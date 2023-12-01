4001c46d100ea4e315d93f4e4546c8e4ebfefecc
Renzo <renzo.abensur@gmail.com>
2023-11-30 23:12:19 -0300

    backup

--- simulation/run_simulations.sh
+++ simulation/run_simulations.sh
@@ -4,13 +4,13 @@ set -x
 #set -v
 
 # Simulation set configuration
-MIN_ITER=2
+MIN_ITER=1
 MAX_ITER=5
 COOJA_INSTANCES=1 #max simulations running in parallel
 COOJA_CURRENT_INSTANCE=1
 
 # nodes_v=(36 100)
-nodes_v=(36)
+nodes_v=(100)
 # topologies=(a1_1 a2_1 a3_1 a1_3 a2_3 a3_3)
 topologies=(a1_3)
 # SIM_TIME_MS=1800000 # 30 minutes
